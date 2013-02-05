2d2c25e3ee18bc5760be21032a88d4361578550d
Mike Redd <mikeredd@mobileinfern.us>
2013-02-05 09:04:43 -0800

    mk files for overlays
    grouper and tuna
    
     Changes to be committed:
    	new file:   config/phone.mk
    	new file:   config/tablet.mk

---
+++ config/phone.mk
@@ -0,0 +1,2 @@
+# Tuna Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/raum00/overlay/tuna
---
+++ config/tablet.mk
@@ -0,0 +1,6 @@
+# grouper overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/raum00/overlay/grouper \
+    vendor/raum00/overlay/common_tablet
+
+ 
