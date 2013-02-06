246309c3e01b19e05db6d35a5ff8431fac30c193
Mike Redd <mikeredd@mobileinfern.us>
2013-02-06 10:50:33 -0800

    Changes to be committed: 	modified:   products/grouper.mk

--- products/grouper.mk
+++ products/grouper.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/asus/grouper/full_grouper.mk)
 
 # Inherit raum00 common_tablet bits
-$(call inherit-product, vendor/raum00/configs/tablet.mk)
+$(call inherit-product, vendor/raum00/config/tablet.mk)
 
 # Grouper Overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/raum00/overlay/grouper
@@ -13,6 +13,7 @@ PRODUCT_BRAND := google
 PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JRO03H/405518:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03H 405518 release-keys" BUILD_NUMBER=405518
 
